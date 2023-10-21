b49798fd2fbff2afd8530b1f8fd242a2c0991991
Liquisafe <Liquisafe@proton.me>
2023-10-21 11:09:49 +0200

    add dex list

--- packages/nextjs/components/constants/dexs.ts
+++ packages/nextjs/components/constants/dexs.ts
@@ -1,68 +1,35 @@
 const dexs = [
   {
-    contract: "factory",
+    contract: "dex",
     platform: "v2",
     name: "uniswap v2",
     addresses: [
       {
         // eth
         chainId: 1,
-        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
       },
       {
         chainId: 31337,
-        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
       },
     ],
   },
   {
-    contract: "erc20",
-    name: "USD Coin ",
-    symbol: "USDC",
-    decimals: 6,
+    contract: "dex",
+    platform: "v3",
+    name: "uniswap v3",
     addresses: [
       {
         // eth
         chainId: 1,
-        address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+        position: "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
       },
       {
         chainId: 31337,
-        address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      },
-    ],
-  },
-  {
-    contract: "erc20",
-    name: "Wrapped BTC",
-    symbol: "WBTC",
-    decimals: 8,
-    addresses: [
-      {
-        // eth
-        chainId: 1,
-        address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-      },
-      {
-        chainId: 31337,
-        address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-      },
-    ],
-  },
-  {
-    contract: "erc20",
-    name: "ChainLink Token",
-    symbol: "LINK",
-    decimals: 18,
-    addresses: [
-      {
-        // eth
-        chainId: 1,
-        address: "0x514910771af9ca656af840dff83e8264ecf986ca",
-      },
-      {
-        chainId: 31337,
-        address: "0x514910771af9ca656af840dff83e8264ecf986ca",
+        factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+        position: "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
       },
     ],
   },
