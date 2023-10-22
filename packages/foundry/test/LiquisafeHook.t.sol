c9ccf2279c0c90665af543ce1394e44ab3c370ab
Liquisafe <Liquisafe@proton.me>
2023-10-22 10:32:43 +0200

    add mint liquidity to test

--- packages/foundry/test/LiquisafeHook.t.sol
+++ packages/foundry/test/LiquisafeHook.t.sol
@@ -16,6 +16,7 @@ import {PoolSwapTest} from "@uniswap/v4-core/contracts/test/PoolSwapTest.sol";
 import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
 import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
 import "../contracts/interfaces/uni-v3/periphery/INonfungiblePositionManager.sol";
+import "../contracts/interfaces/uni-v3/core/IUniswapV3Factory.sol";
 
 contract TestLiquisafeHook is Test, Deployers, TokenFixture {
     using PoolIdLibrary for PoolKey;
@@ -25,8 +26,14 @@ contract TestLiquisafeHook is Test, Deployers, TokenFixture {
     INonfungiblePositionManager public constant uniswapV3PositionManager =
         INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
+    IUniswapV3Factory public constant uniswapV3Factory =
+        IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+
     TestERC20 token0;
     TestERC20 token1;
+
+    uint24 poolFee = 500;
+
     PoolManager manager;
     LiquisafeHook liquisafeHook =
         LiquisafeHook(
@@ -38,6 +45,10 @@ contract TestLiquisafeHook is Test, Deployers, TokenFixture {
     PoolId id;
 
     PoolSwapTest swapRouter;
+
+    address deployer = makeAddr("Deployer");
+    address alice = makeAddr("Alice");
+    address bob = makeAddr("Bob");
 
     function setUp() public {
         vm.createSelectFork("mainnet");
@@ -74,9 +85,44 @@ contract TestLiquisafeHook is Test, Deployers, TokenFixture {
 
         swapRouter = new PoolSwapTest(manager);
 
+        uniswapV3Factory.createPool(address(token0), address(token1), poolFee);
+
         token0.approve(address(liquisafeHook), type(uint256).max);
         token1.approve(address(liquisafeHook), type(uint256).max);
         token0.approve(address(swapRouter), type(uint256).max);
         token1.approve(address(swapRouter), type(uint256).max);
+        token0.approve(address(uniswapV3PositionManager), type(uint256).max);
+        token1.approve(address(uniswapV3PositionManager), type(uint256).max);
+    }
+
+    function TestAddLiquidity() public {
+        // uniswapV3Factory.createPool(token0,token1)
+        _mintPosition(1500 * 10 ** 6, 10 ** 6, alice);
+    }
+
+    function _mintPosition(
+        uint256 amount0ToMint,
+        uint256 amount1ToMint,
+        address recipient
+    ) private returns (uint256) {
+        INonfungiblePositionManager.MintParams
+            memory params = INonfungiblePositionManager.MintParams({
+                token0: address(token0),
+                token1: address(token1),
+                fee: poolFee,
+                tickLower: TickMath.MIN_TICK,
+                tickUpper: TickMath.MAX_TICK,
+                amount0Desired: amount0ToMint,
+                amount1Desired: amount1ToMint,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: recipient,
+                deadline: block.timestamp
+            });
+
+        // Note that the pool defined by DAI/USDC and fee tier 0.3% must already be created and initialized in order to mint
+        (uint256 tokenId, , , ) = uniswapV3PositionManager.mint(params);
+
+        return tokenId;
     }
 }
